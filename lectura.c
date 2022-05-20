b83828a25ca22ccff531a1e335391a4c7c23372e
NicolasLopez147 <nilopezn@unal.edu.co>
2022-05-20 17:03:26 -0500

    Tuberias

--- lectura.c
+++ lectura.c
@@ -7,26 +7,60 @@
 
 int main(){
     //Descriptor del archivo
-    int t;
+    int descriptorTuberia;
+
+    int tamano = 10000*sizeof(char);
+
     //Ruta del archivo tuberia
     char *tuberia = "./tuberia"; 
 
-    char * buffer = malloc(sizeof(char)*1000*pow(10,1));
+    char * buffer = malloc(tamano);
 
     //Se abre la tuberia
-    t = open(tuberia,O_RDONLY);
-    if (t < 0){
+    descriptorTuberia = open(tuberia,O_RDONLY);
+    if (descriptorTuberia < 0){
         perror("Hubo un error abriendo el archivo de la tuberia");
         exit(-1);
     }
+    int bandera = 1;
+    int contador = 0;
+    while(bandera)
+    {   
+        
+        //Se lee el mensaje de la tuberia
+        int r = read(descriptorTuberia,buffer,tamano);
+        if (r < 0){
+            perror("Hubo un error leyendo el archivo de la tuberia");
+            exit(-1);
+        }
+        printf("Se leyeron %d bytes\n",r);
+        printf("Ultimo byte leido %s\n",(buffer+r));
+        if (r == 0){
+            contador++;
+        }
+        if (contador > 20){
+            bandera = 0;
+        }
 
-    //Se lee el mensaje de la tuberia
-    int r = read(t,buffer,sizeof(buffer));
-    if (r < 0){
-        perror("Hubo un error leyendo el archivo de la tuberia");
-        exit(-1);
     }
     
-    close(t);
-    return 0;
+    // for (int potencia ; potencia < 6 ; potencia ++){
+    //     tamano = 1000*pow(10,potencia)*sizeof(char);
+
+    //     char * buffer = malloc(tamano);
+
+    //     //Se lee el mensaje de la tuberia
+    //     int r = read(descriptorTuberia,buffer,tamano);
+        
+    //     if (r < 0){
+    //         perror("Hubo un error leyendo el archivo de la tuberia");
+    //         exit(-1);
+    //     }
+    //     // if (*(buffer+r) == 'a')
+    //     //     printf("Ultimo byte leido %s\n",(buffer+r));
+
+    // }
+    
+    close(descriptorTuberia);
+    return 0;   
 }
--- main.c
+++ main.c
@@ -16,25 +16,30 @@ int main(){
     //Crear un archivo de tipo fifo
     mkfifo(tuberia,0666);
     
-    //Se abre la tuberia en modo de escritura
-    descriptorTuberia = open(tuberia,O_WRONLY);
+    for (int potencia = 0 ; potencia < 6;potencia ++){
+        //Se abre la tuberia en modo de escritura
+        descriptorTuberia = open(tuberia,O_WRONLY);
 
-    if (descriptorTuberia < 0){
-        perror("Hubo un error abriendo el archivo de la tuberia");
-        exit(-1);
-    }
-    for (int potencia = 1 ; potencia < 2;potencia ++){
+        if (descriptorTuberia < 0){
+            perror("Hubo un error abriendo el archivo de la tuberia");
+            exit(-1);
+        }   
+
         int tamano = 1000*pow(10,potencia)*sizeof(char);
         //Se aparta memoria de la informacion a enviar
         char * buffer = malloc(tamano);
+        *(buffer+tamano) = 'a';
         int r = write(descriptorTuberia,buffer,tamano);
         if (r < 0){
             perror("Hubo un error enviando los datos");
             exit(-1);
         }
-        printf("Tamano mensaje enviado %d",r);
+        printf("Tamano mensaje enviado %d\n",r);
+        // printf("Ultimo elemento enviado %s\n",(buffer+r));
+
+        close(descriptorTuberia);
+
     }
-    close(descriptorTuberia);
     unlink(tuberia);
     return 0;
 }
