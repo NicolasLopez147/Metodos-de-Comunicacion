6b02a27b619ad31fb4925ac02ccb6f6ee0d9cf20
Fabián Torres <fatorresra@unal.edu.co>
2022-05-25 21:11:37 -0500

    Actualizar script.sh con clean

--- script.sh
+++ script.sh
@@ -8,28 +8,32 @@ function funTuberia(){
     echo "-----------------------Tuberia-------------------------"
     cd $tuberia
     make
+    make clean
     cd ..
 }
 function funPaso(){
     echo "--------------------Paso por mensaje-------------------"
     cd $pasoMensaje
     make
+    make clean
     cd ..
 }
 function funArchivos(){
     echo "-----------------------Archivos------------------------"
     cd $archivos
     make
+    make clean
     cd ..
 }
 function funMemCompartida(){
     echo "-------------------Memoria compartida------------------"
     cd $memCompartida
     make
+    make clean
     cd ..
 }
-funTuberia
-funPaso
 funArchivos
 funMemCompartida
+funTuberia
+funPaso
 exec bash
